cdcfa98b84a8a28dd293f393d9209f3dcc79fa8c
Estelita <estelitac24401@hotmail.com>
2024-04-06 15:30:38 -0700

    re structuring scheduling

---
+++ src/scheduling/AlphaShift.java
@@ -0,0 +1,12 @@
+package scheduling;
+
+public class AlphaShift extends Shift{
+
+    public AlphaShift(Department d) {
+        super(d);
+        this.shiftName = "Alpha";
+        this.startTime = 600;
+        this.endTime = 1200;
+        this.length = 6;
+    }
+}
---
+++ src/scheduling/Shift.java
@@ -0,0 +1,55 @@
+package scheduling;
+
+import personnel.ICrewMember;
+import java.time.Duration;
+import java.sql.Time;
+
+
+public abstract class Shift {
+    String shiftName;
+    int startTime;
+    int endTime;
+    double length;
+    Department location;
+
+    public Shift(Department d){
+        this.location = d;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("%s Shift: %s\n\t%d - %d\n", shiftName, location, startTime, endTime);
+    }
+
+    public Department getDepartment(){
+        return this.location;
+    }
+
+    public void setDepartment(Department newDepartment){
+        this.location = newDepartment;
+    }
+
+    public double getNumHours(){
+        return this.length;
+    }
+
+    double calculateDuration(){
+        //TODO
+        return 0;
+    }
+
+    boolean validateNewTime(){
+        //return true;//if shift wouldnt be too long
+        return false; //if shift is too long
+    }
+
+    public void changeStart(){
+        //TODO
+        this.updateNumHours();
+    }
+
+    public void changeEnd(){
+        //TODO
+        this.updateNumHours();
+    }
+}
