4927bcc74a6f183c75b6532f02cb8a03560fb9e6
Estelita <estelitac24401@hotmail.com>
2024-04-09 16:03:50 -0700

    adding toString for species

--- src/model/personnel/Species.java
+++ src/model/personnel/Species.java
@@ -1,5 +1,21 @@
 package model.personnel;
 
 public enum Species {
-HUMAN, VULCAN, KLINGON, ROMULAN, BAJORAN, ANDORIAN, FERENGI, TRILL, BETAZOID
+HUMAN, VULCAN, KLINGON, ROMULAN, BAJORAN, ANDORIAN, FERENGI, TRILL, BETAZOID;
+
+    @Override
+    public String toString() {
+        switch (this) {
+            case HUMAN:return "Human";
+            case VULCAN:return "Vulcan";
+            case KLINGON:return "Klingon";
+            case ROMULAN:return "Romulan";
+            case BAJORAN:return "Bajoran";
+            case ANDORIAN:return "Andorian";
+            case FERENGI:return "Ferengi";
+            case TRILL:return "Trill";
+            case BETAZOID:return "Betazoid";
+            default: return "Unknown Species";
+        }
+    }
 }
