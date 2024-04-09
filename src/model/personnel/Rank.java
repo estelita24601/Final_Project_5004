fd150acfebc0ab5fe036a8c4fbbab27d4962bf28
Estelita <estelitac24401@hotmail.com>
2024-04-09 15:56:22 -0700

    creating rank enum and adding features so they can be compared and printed out easier

--- src/model/personnel/Rank.java
+++ src/model/personnel/Rank.java
@@ -1,5 +1,44 @@
 package model.personnel;
 
 public enum Rank {
-    TECHNICIAN, PETTY_OFFICER, ENSIGN, LIEUTENANT_JR_GRADE, LIEUTENANT, LIEUTENANT_COMMANDER, COMMANDER, CAPTAIN, ADMIRAL
-}
+    TECHNICIAN(0),
+    PETTY_OFFICER(1),
+    CHIEF_PETTY_OFFICER(1.5),
+    ENSIGN(2),
+    LIEUTENANT_JR_GRADE(3),
+    LIEUTENANT(3.25),
+    LIEUTENANT_COMMANDER(4),
+    COMMANDER(4.5),
+    CAPTAIN(5),
+    ADMIRAL(6);
+
+
+    private double rank_hierarchy;
+
+    Rank(double i) {
+        this.rank_hierarchy = i;
+    }
+
+    @Override
+    public String toString() {
+        switch(this){
+            case TECHNICIAN: return "Technician";
+            case PETTY_OFFICER: return "Petty Officer";
+            case CHIEF_PETTY_OFFICER: return "Chief Petty Officer";
+            case ENSIGN: return "Ensign";
+            case LIEUTENANT_JR_GRADE: return "Lieutenant Jr. Grade";
+            case LIEUTENANT: return "Lieutenant";
+            case LIEUTENANT_COMMANDER: return "Lieutenant Commander";
+            case COMMANDER: return "Commander";
+            case CAPTAIN: return "Captain";
+            case ADMIRAL: return "Admiral";
+            default: return "Unknown Rank";
+        }
+    }
+
+    public double getRankValue(){
+        return this.rank_hierarchy;
+    }
+
+
+    }
