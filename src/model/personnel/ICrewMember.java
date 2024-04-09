d9ad388119e019ff4bd266c06c1e30e084e26d63
Estelita <estelitac24401@hotmail.com>
2024-04-09 16:10:13 -0700

    fleshing out crew member interface

--- src/model/personnel/ICrewMember.java
+++ src/model/personnel/ICrewMember.java
@@ -1,13 +1,29 @@
 package model.personnel;
 
+import model.scheduling.Department;
+import model.scheduling.Shift;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 public interface ICrewMember {
     void promote(Rank newRank);
     void demote(Rank newRank);
+    Rank getRank();
+
+    void changeName(String newName);
     String getName();
+
+    void setHeritage(ArrayList<Species> heritage);
+    void addToHeritage(Species newSpecies);
+    void removeFromHeritage(Species newSpecies);
     List<String> getHeritage();
 
+    void editShift(Consumer<Shift> shiftEditor);
+    void setShift(Shift newShift);
+    Shift getShift();
 
+    void setJob(Department newDepartment);
+    Department getJob();
 }
