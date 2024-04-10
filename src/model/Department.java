edf8b5d9b6197cd83dc5553eb34953622266e88b
Estelita <estelitac24401@hotmail.com>
2024-04-09 17:17:33 -0700

    package change

---
+++ src/model/Department.java
@@ -0,0 +1,33 @@
+package model;
+
+public enum Department {
+    ENGINEERING, BRIDGE, SECURITY, SCIENCE, MEDICAL_BAY;
+
+    @Override
+    public String toString() {
+        String output = null;
+        switch (this) {
+            case ENGINEERING -> {
+                output = "Engineering";
+            }
+            case BRIDGE -> {
+                output = "Bridge";
+            }
+            case SECURITY -> {
+                output = "Security";
+            }
+            case SCIENCE -> {
+                output = "Science Department";
+            }
+            case MEDICAL_BAY -> {
+                output = "Medical Bay";
+            }default -> {
+                throw new IllegalStateException("Invalid value for department enum");
+            }
+        }
+
+        return output;
+    }
+
+
+}
