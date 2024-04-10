cf03ee8ac4cf2077d36578b95b000822afd640fe
Estelita <estelitac24401@hotmail.com>
2024-04-09 17:21:12 -0700

    making model

---
+++ src/model/CrewModel.java
@@ -0,0 +1,53 @@
+package model;
+
+import java.util.ArrayList;
+
+import model.personnel.ICrew;
+import model.personnel.ICrewMember;
+import model.scheduling.Shift;
+
+public class CrewModel implements IModel {
+    ArrayList<String> rankOptions;
+    ArrayList<String> speciesOptions;
+    ArrayList<String> departmentOptions;
+    ICrew<ICrewMember> crew;
+
+    // need a constructor!!
+
+    @Override
+    public ArrayList<String> getRankOptions() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRankOptions'");
+    }
+
+    @Override
+    public ArrayList<String> getSpeciesOptions() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSpeciesOptions'");
+    }
+
+    @Override
+    public ArrayList<String> getDepartmentOptions() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDepartmentOptions'");
+    }
+
+    @Override
+    public ArrayList<String> getShiftOptions() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getShiftOptions'");
+    }
+
+    @Override
+    public ICrewMember creatCrewMember() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'creatCrewMember'");
+    }
+
+    @Override
+    public Shift createShift() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createShift'");
+    }
+
+}
