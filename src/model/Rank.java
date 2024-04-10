0a79ad1dac6a5da9a8c276b93a863fe742ad3f86
Estelita <estelitac24401@hotmail.com>
2024-04-10 14:10:54 -0700

    simplifying rank enum

--- src/model/Rank.java
+++ src/model/Rank.java
@@ -1,22 +1,20 @@
 package model;
 
 public enum Rank {
-    TECHNICIAN(0, "Technician"),
-    PETTY_OFFICER(1, "Petty Officer"),
-    CHIEF_PETTY_OFFICER(1.5, "Chief Petty Officer"),
-    ENSIGN(2, "Ensign"),
-    LIEUTENANT_JR_GRADE(3, "Lieutenant Jr. Grade"),
-    LIEUTENANT(3.25, "Lieutenant"),
-    LIEUTENANT_COMMANDER(4, "Lieutenant Commander"),
-    COMMANDER(4.5, "Commander"),
-    CAPTAIN(5, "Captain"),
-    ADMIRAL(6, "Admiral");
+    TECHNICIAN("Technician"),
+    PETTY_OFFICER("Petty Officer"),
+    CHIEF_PETTY_OFFICER("Chief Petty Officer"),
+    ENSIGN("Ensign"),
+    LIEUTENANT_JR_GRADE("Lieutenant Jr. Grade"),
+    LIEUTENANT("Lieutenant"),
+    LIEUTENANT_COMMANDER("Lieutenant Commander"),
+    COMMANDER("Commander"),
+    CAPTAIN("Captain"),
+    ADMIRAL("Admiral");
 
-    public final double rank_value;
     public final String rank_name;
 
-    Rank(double i, String name) {
-        this.rank_value = i;
+    Rank(String name) {
         this.rank_name = name;
     }
 
