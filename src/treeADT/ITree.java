3937841beb860ce6b43359381083baa75e6e4cf9
Estelita <estelitac24401@hotmail.com>
2024-04-11 15:35:00 -0700

    refactoring tree ADT

---
+++ src/treeADT/ITree.java
@@ -0,0 +1,53 @@
+package treeADT;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public interface ITree<T> {
+    T getData();
+
+    void setData(T newData);
+
+    void editData(Consumer<T> editor);
+
+    TreeNode<T> getParent();
+
+    void setParent(TreeNode<T> newParent);
+
+    ArrayList<TreeNode<T>> getChildren();
+
+    //ABSTRACT METHODS//////////////////////////////////////////////////////////////////
+    void moveChildren(Predicate<T> findChildrenToReassign, BranchNode<T> newParent);
+
+    void moveChildren(BranchNode<T> newParent);
+
+    int countIf(Predicate<T> filter);
+
+    int countAll();
+
+    List<T> toList();
+
+    boolean addChild(TreeNode<T> newChild);
+
+    boolean addChild(T newChildData, Predicate<T> canBeBranch);
+
+    boolean deleteChild(Predicate<T> findChildToDelete);
+
+    boolean deleteChild(TreeNode<T> childToDelete);
+
+    TreeNode<T> deepCopy();
+
+    TreeNode<T> findNode(Predicate<T> identifier);
+
+    <R> R fold(R initial, BiFunction<R, T, R> combiner);
+
+    List<T> filterToList(Predicate<T> filter);
+
+    <R> TreeNode<R> map(Function<T, R> converter);
+
+    <R> List<R> mapToList(Function<T, R> converter);
+}
--- src/treeADT/TreeNode.java
+++ src/treeADT/TreeNode.java
@@ -1,13 +1,10 @@
 package treeADT;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.function.BiFunction;
 import java.util.function.Consumer;
-import java.util.function.Function;
 import java.util.function.Predicate;
 
-public abstract class TreeNode<T> {
+public abstract class TreeNode<T> implements ITree<T> {
     TreeNode<T> parent;
     T data;
     ArrayList<TreeNode<T>> children;
@@ -23,18 +20,22 @@ public abstract class TreeNode<T> {
 
     //CONCRETE METHODS////////////////////////////////////////////////////////////////////////
 
+    @Override
     public T getData() {
         return this.data;
     }
 
+    @Override
     public void setData(T newData) {
         this.data = newData;
     }
 
+    @Override
     public void editData(Consumer<T> editor) {
         editor.accept(this.data);
     }
 
+    @Override
     public TreeNode<T> getParent() {
         if (this.parent != null) {
             return this.parent;
@@ -43,6 +44,7 @@ public abstract class TreeNode<T> {
         }
     }
 
+    @Override
     public void setParent(TreeNode<T> newParent) {
         Predicate<T> findChildToDelete = (t) -> {return t.equals(this);};
         this.parent.deleteChild(findChildToDelete); // remove ourselves from the parent's list of children
@@ -50,36 +52,10 @@ public abstract class TreeNode<T> {
         this.parent.addChild(this); //add ourselves to new parent's list of children
     }
 
+    @Override
     public ArrayList<TreeNode<T>> getChildren() {
         return this.children;
     }
 
 
-    //ABSTRACT METHODS//////////////////////////////////////////////////////////////////
-    public abstract void moveChildren(Predicate<T> findChildrenToReassign, BranchNode<T> newParent);
-    public abstract void moveChildren(BranchNode<T> newParent);
-
-    public abstract int countIf(Predicate<T> filter);
-
-    public abstract int countAll();
-
-    public abstract List<T> toList();
-
-    public abstract boolean addChild(TreeNode<T> newChild);
-    public abstract boolean addChild(T newChildData, Predicate<T> canBeBranch);
-
-    public abstract boolean deleteChild(Predicate<T> findChildToDelete);
-    public abstract boolean deleteChild(TreeNode<T> childToDelete);
-
-    public abstract TreeNode<T> deepCopy();
-
-    public abstract TreeNode<T> findNode(Predicate<T> identifier);
-
-    public abstract <R> R fold(R initial, BiFunction<R, T, R> combiner);
-
-    public abstract List<T> filterToList(Predicate<T> filter);
-
-    public abstract <R> TreeNode<R> map(Function<T, R> converter);
-
-    public abstract <R> List<R> mapToList(Function<T, R> converter);
 }
