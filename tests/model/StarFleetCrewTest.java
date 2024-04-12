d9566e3b9556413a286fe27d8350fc3057a25e6d
Estelita <estelitac24401@hotmail.com>
2024-04-11 18:27:22 -0700

    generating stub for test class

---
+++ tests/model/StarFleetCrewTest.java
@@ -0,0 +1,79 @@
+package model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StarFleetCrewTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void setRoot() {
+    }
+
+    @Test
+    public void getRankOptions() {
+    }
+
+    @Test
+    public void getSpeciesOptions() {
+    }
+
+    @Test
+    public void getShiftRotationOptions() {
+    }
+
+    @Test
+    public void getDepartmentOptions() {
+    }
+
+    @Test
+    public void countAll() {
+    }
+
+    @Test
+    public void countFilter() {
+    }
+
+    @Test
+    public void getMemberList() {
+    }
+
+    @Test
+    public void testGetMemberList() {
+    }
+
+    @Test
+    public void getMemberInfoList() {
+    }
+
+    @Test
+    public void getCrewMember() {
+    }
+
+    @Test
+    public void getCrewMemberInfo() {
+    }
+
+    @Test
+    public void editCrewMember() {
+    }
+
+    @Test
+    public void addCrewMember() {
+    }
+
+    @Test
+    public void removeCrewMember() {
+    }
+
+    @Test
+    public void reAssignTo() {
+    }
+
+    @Test
+    public void putInCommandOf() {
+    }
+}
