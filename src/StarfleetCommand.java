997d704d21092140940d7a66474621df89a55117
Estelita <estelitac24401@hotmail.com>
2024-04-13 00:58:23 -0700

    finished refactoring getValidChoice and used it to fix species selection menu

--- src/StarfleetCommand.java
+++ src/StarfleetCommand.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.function.Function;
 
-//note: default commands will either be integer cgiuces
 public class StarfleetCommand implements ICrewController {
+    \
     final Readable in;
     private Scanner scanner;
     private ICrewModel<ICrewMember> model;
@@ -20,7 +20,7 @@ public class StarfleetCommand implements ICrewController {
         boolean exit = false;
         scanner = new Scanner(in);
 
-        view.welcomeMessage();
+        this.view.welcomeMessage();
 
         createCaptain();
 
@@ -40,97 +40,75 @@ public class StarfleetCommand implements ICrewController {
             } else if (menuChoice == -1) {
                 exit = true;
             }
-            //if they didn't decide to exit go back to main menu after they finished what they were doing
+            // if they didn't decide to exit go back to main menu after they finished what
+            // they were doing
         }
 
-        view.goodbyeMessage();
+        this.view.goodbyeMessage();
     }
 
-    /**
-     * @param converter    (Function<String,T>) - lambda that will take user input and attempt to convert it to desired data type
-     * @param viewPrompter (ViewDisplayer) - lambda that will call the correct view methods for the given context
-     * @return T - the user input converted to desired type
-     * @return null - if the user decided to quit and we were unable to get valid value from them
-     */
-    private <T> T getValidInput(Function<String, T> converter, ViewDisplayer viewPrompter) {
-        boolean validInput = false;
-        T result = null;
-
-        while (!validInput) {
-            viewPrompter.display(); //get the view to show whatever message we need
-            String input = scanner.nextLine(); //get the user's input
-            input = input.strip();
-
-            //first make sure they don't want to leave
-            if (commandDetected(input, "quit")) {
-                validInput = true;
-            }
-
-            try {
-                result = converter.apply(input);
-                validInput = true; //leave loop we were able to convert the input to correct data type
-            } catch (IllegalArgumentException e) {
-                this.view.displayError(e);
-                this.view.displayTryAgainMessage();
-            }
-        }
-
-        return result;
-    }
-
-    //NOTE: trying to replace getValidInput with this instead
-    //view will present numbered options and we just need to make sure we got valid number, no more words
-    //IDEA: maybe return object from options list that was selected instead? and return null for exit command
-    private int getValidChoice(Object[] options, ViewDisplayer choicePrompt, ViewDisplayer invalidResponse){
+    // view will present numbered options and we just need to make sure we got valid
+    // number, no more words
+    // IDEA: maybe return object from options list that was selected instead? and
+    // return null for exit command
+    private Object getValidChoice(Object[] options, ViewDisplayer choicePrompt, ViewDisplayer invalidResponse) {
+        int choiceNumber;
+        Object chosenOption = null;
         boolean receivedValidChoice = false;
-        while(!receivedValidChoice){
+
+        while (!receivedValidChoice) {
             choicePrompt.display();
             view.displayOptions(options);
             String userInput = scanner.nextLine().strip();
-            int choice; 
-            try{
-                choice = Integer.valueOf(userInput);
-            }catch (){ //todo: look up exact name of error
-                //didn't even receive a number
-                invalidResponse.display();
-            }
-            if(choice == -1 || choice < options.length){
+
+            // now see if input corresponds to an option in the list
+            try {
+                choiceNumber = Integer.valueOf(userInput);
+                chosenOption = options[choiceNumber];
                 receivedValidChoice = true;
-            }else{
-                //number received wasn't one of the options
-                invalidResponse.display(); //todo: improve how this fits with try catch
-                //IDEA: maybe try to access an array with the choice # and use index out of bounds error for too big #
+            } catch (NumberFormatException e) {
+                // didn't even receive a number
+                invalidResponse.display();
+            } catch (ArrayIndexOutOfBoundsException e) {
+                // check if they tried to exit
+                if (choiceNumber == -1) {
+                    receivedValidChoice = true;
+                } else {
+                    // otherwise number given was just wrong
+                    invalidResponse.display();
+                }
             }
-        }    
-        return choice;   
+        }
+        return chosenOption;
     }
 
-    private int getValidChoice(ViewDisplayer choicePrompt, ViewDisplayer invalidResponse, int numOptions){
+    private int getValidChoice(ViewDisplayer choicePrompt, ViewDisplayer invalidResponse, int numOptions) {
+        int choice;
         boolean receivedValidChoice = false;
-        while(!receivedValidChoice){
+
+        while (!receivedValidChoice) {
             choicePrompt.display();
             String userInput = scanner.nextLine().strip();
-            int choice; 
-            try{
+            try {
                 choice = Integer.valueOf(userInput);
-            }catch (){ //todo: look up exact name of error
-                //didn't even receive a number
+            } catch (NumberFormatException e) {
+                // didn't even receive a number
                 invalidResponse.display();
             }
-            if(choice == -1 || choice < numOptions){
+            if (choice == -1 || choice < numOptions) {
                 receivedValidChoice = true;
-            }else{
-                //number received wasn't one of the options
-                invalidResponse.display(); //todo: improve how this fits with try catch
+            } else {
+                // number received wasn't one of the options
+                invalidResponse.display();
             }
-        }    
-        return choice;   
+        }
+        return choice;
     }
-
 
     private void createCaptain() {
         boolean invalidInput = true;
         boolean quitEarly = false;
+        ViewDisplayer askToTryAgain = () -> view.displayTryAgainMessage();
 
         while (invalidInput) {
             view.askForCaptain();
@@ -138,15 +116,18 @@ public class StarfleetCommand implements ICrewController {
             view.askForName();
             String name = scanner.nextLine().strip();
 
-            Function<String, Rank> rankConverter = (str) -> convertInputToEnum(str, model.getRankOptions()); //lambda that tries to convert input to rank enum
-            ViewDisplayer askForRank = () -> view.askForRank(); //lambda that gets the view to ask for a rank
-            Rank rank = getValidInput(rankConverter, askForRank); //give two lambdas to helper that will run loop until receiving valid input or until user quits
-            //if getValidInput returned null then user wants to exit early
+            //todo: fix rank
+            Function<String, Rank> rankConverter = (str) -> convertInputToEnum(str, model.getRankOptions());
+            ViewDisplayer askForRank = () -> view.askForRank(); // lambda that gets the view to ask for a rank
+            Rank rank = getValidInput(rankConverter, askForRank); // give two lambdas to helper that will run loop until
+            // receiving valid input or until user quits
+            // if getValidInput returned null then user wants to exit early
             if (rank == null) {
                 quitEarly = true;
                 break;
             }
 
+            //todo: fix rotation
             Function<String, Rotation> rotationConverter = (str) -> convertInputToEnum(str, model.getShiftRotationOptions());
             ViewDisplayer askForRotation = () -> view.askForRotation();
             Rotation rotation = getValidInput(rotationConverter, askForRotation);
@@ -156,26 +137,19 @@ public class StarfleetCommand implements ICrewController {
                 break;
             }
 
-            //species is optional so check
-            Function<String, Boolean> decisionConverter = (str) -> checkYesOrNo(str);
             ViewDisplayer askForSpecies = () -> view.askToDiscloseSpecies();
-            boolean discloseSpecies = getValidInput(decisionConverter, askForSpecies);
+            Boolean[] yesOrNo = {true, false};
+            boolean discloseSpecies = (Boolean) getValidChoice(yesOrNo, askForSpecies, askToTryAgain);
 
             ArrayList<Species> heritage;
             if (discloseSpecies) {
-                heritage = runSpeciesSelectionMenu(); //allow them to continue adding species until they stop or quit
-
-                // check if the user wants to quit early
-                if (heritage == null) {
-                    quitEarly = true;
-                    break;
-                }
+                heritage = runSpeciesSelectionMenu(); // allow them to continue adding species until they stop or quit
             }
 
             try {
-                StarFleetOfficer captain = new StarFleetOfficer(name, rank, Department.BRIDGE, rotation);
+                StarFleetOfficer captain = new StarFleetOfficer(name, rank, Department.BRIDGE, rotation, heritage);
                 this.model.setRoot(captain);
-                invalidInput = false; //only break out of the loop if succesfully able to set root for the crew
+                invalidInput = false; // only break out of the loop if succesfully able to set root for the crew
             } catch (IllegalArgumentException e) {
                 view.displayError(e);
             }
@@ -185,22 +159,22 @@ public class StarfleetCommand implements ICrewController {
     private ArrayList<Species> runSpeciesSelectionMenu() {
         ArrayList<Species> heritage = new ArrayList<>();
 
-        boolean finished = false;
-        while (!finished) {
-            Function<String, Species> speciesConverter = (str) -> convertInputToEnum(str, model.getSpeciesOptions());
+        boolean wantToContinue = true;
+        while (wantToContinue) {
             ViewDisplayer askForSpecies = () -> view.askForSpecies();
-            Species current = getValidInput(speciesConverter, askForSpecies);
+            ViewDisplayer errorMessage = () -> view.displayTryAgainMessage();
+            Species currentSpecies = (Species) getValidChoice(model.getSpeciesOptions(), askForSpecies, errorMessage);
 
-            if (current == null) {
-                //they want to quit early
-                return null;
+            if (currentSpecies == null) {
+                // they want to quit early
+                break;
             }
-            heritage.add(current);
+            heritage.add(currentSpecies);
 
-            //check for exit condition
-            Function<String, Boolean> convertDecision = (str) -> checkYesOrNo(str);
-            ViewDisplayer askIfFinished = () -> view.askIfFinishedGivingHeritage();
-            finished = getValidInput(convertDecision, askIfFinished);
+            //check if they want to continue or want to stop
+            Boolean[] yesOrNo = {true, false};
+            ViewDisplayer askIfContinue = () -> view.askIfWantToContinueGivingSpecies();
+            wantToContinue = (Boolean) getValidChoice(yesOrNo, askIfContinue, errorMessage);
         }
 
         return heritage;
