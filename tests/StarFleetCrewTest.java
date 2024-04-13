4c19a6a3b44a3ba1b8aa6a560b705c24fb1f52ff
Estelita <estelitac24401@hotmail.com>
2024-04-13 16:37:34 -0700

    writing tests, found issue with find node method

--- tests/StarFleetCrewTest.java
+++ tests/StarFleetCrewTest.java
@@ -1,46 +1,46 @@
 import org.junit.Before;
 import org.junit.Test;
+import treeADT.TreeNode;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static org.junit.Assert.assertEquals;
 
 public class StarFleetCrewTest {
+    StarFleetOfficer testOfficer;
     StarFleetCrew voyagerCrew;
+    StarFleetOfficer janeway;
+    StarFleetOfficer belanna;
     StarFleetCrew deepSpaceNineCrew;
+    StarFleetOfficer sisko;
+    StarFleetOfficer dax;
 
     @Before
     public void setUp() throws Exception {
+        testOfficer = new StarFleetOfficer("Jane Doe", Rank.ENSIGN, Department.SECURITY, Rotation.DELTA);
         voyagerCrew = new StarFleetCrew();
-
-    }
-
-    @Test
-    public void getRankOptions() {
-    }
-
-    @Test
-    public void getSpeciesOptions() {
+        janeway = new StarFleetOfficer("Kathryn Janeway", Rank.CAPTAIN, Department.BRIDGE, Rotation.ALPHA, Species.HUMAN);
+        ArrayList<Species> belannaHeritage = new ArrayList<>(List.of(Species.HUMAN, Species.KLINGON));
+        belanna = new StarFleetOfficer("B'Elanna Torres", Rank.LIEUTENANT_JR_GRADE, Department.ENGINEERING, Rotation.BETA, belannaHeritage);
+        deepSpaceNineCrew = new StarFleetCrew();
+        sisko = new StarFleetOfficer("Benjamin Sisko", Rank.CAPTAIN, Department.BRIDGE, Rotation.ALPHA, Species.HUMAN);
+        dax = new StarFleetOfficer("Jadzia Dax", Rank.LIEUTENANT_COMMANDER, Department.SCIENCE, Rotation.GAMMA, Species.TRILL);
     }
 
     @Test
     public void setRoot() {
-    }
-
-    @Test
-    public void getShiftRotationOptions() {
-    }
-
-    @Test
-    public void getDepartmentOptions() {
+        voyagerCrew.setRoot(janeway);
+        assertEquals(janeway, voyagerCrew.getRoot().getData());
     }
 
     @Test
     public void loadFromFile() throws FileNotFoundException {
+        //FIXME: only loading in the first officer in the file
         voyagerCrew.loadFromFile("resources/VoyagerCrew.csv");
-        System.out.println(voyagerCrew);
-    }
-
-    @Test
-    public void testToString() {
+        assert (false);
     }
 
     @Test
@@ -77,6 +77,12 @@ public class StarFleetCrewTest {
 
     @Test
     public void addCrewMember() {
+        voyagerCrew.setRoot(janeway);
+        Predicate<ICrewMember> findJaneway = (officer) -> "Kathryn Janeway".equalsIgnoreCase(officer.getName());
+        voyagerCrew.addCrewMember(belanna, findJaneway);
+
+        ArrayList<TreeNode<ICrewMember>> janewaySubordinates = voyagerCrew.getRoot().getChildren();
+        assertEquals(1, janewaySubordinates.size());
     }
 
     @Test
@@ -92,6 +98,6 @@ public class StarFleetCrewTest {
     }
 
     @Test
-    public void getRoot() {
+    public void testToString() {
     }
 }
