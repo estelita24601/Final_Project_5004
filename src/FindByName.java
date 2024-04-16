e4db7714c0623c49fd81105a4cbd491a94f58d1f
Estelita <estelitac24401@hotmail.com>
2024-04-16 00:39:19 -0700

    adding doc comment

--- src/FindByName.java
+++ src/FindByName.java
@@ -1,9 +1,13 @@
+/**
+ * Functional class that extends predicate so we can more easily create predicates that find crew members by name
+ */
+
 import model.ICrewMember;
 
 import java.util.function.Predicate;
 
 public class FindByName implements Predicate<ICrewMember> {
-    private String name;
+    private final String name;
 
     public FindByName(String nameWeWantToFind) {
         this.name = nameWeWantToFind;
